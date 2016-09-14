002806aa2d562608c3cbd3b902f28f70efe69520
Antonio Rodrigues <emailhealthquestsolutions@gmail.com>
2016-09-14 13:39:27 +0100

    File cleanup

--- conf/db/migration/dbname/V2__Add_people.sql
+++
@@ -1,4 +0,0 @@
-insert into PERSON (ID, NAME) values (1, 'For Junior');
-insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
-insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
-insert into PERSON (ID, NAME) values (4, 'Josh');
